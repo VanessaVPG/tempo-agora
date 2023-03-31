8678d0145814b254f0fff609d5eb1cb4cecb6d78
vanessavpg <vanessa.pg026@gmail.com>
2023-03-31 17:39:41 -0300

    fix {}

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -60,7 +60,7 @@
 
 <script setup>
 import { ref } from "vue";
-import { uid } from "uid";
+import uid  from "uid";
 import { RouterLink, useRoute, useRouter } from "vue-router";
 import BaseModal from "./BaseModal.vue";
 
